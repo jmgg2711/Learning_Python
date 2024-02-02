8e300a6be5a7d9ed0e8aebfc8dd6d165ee124c86
Juan Gutierrez <juan-manuel.gutierrez@infineon.com>
2024-02-02 14:07:43 -0800

    alcance

--- Python/Funciones/05-alcance.py
+++ Python/Funciones/05-alcance.py
@@ -1,15 +1,17 @@
 saludo = "HOLA GLOBAL"
 
+
 def saludar():
     global saludo
     saludo = "Hola Mundo"
     print(saludo)
-    
-    
+
+
 def saludaChanchito():
     saludo = "Hola Chanchito"
     print(saludo)
-    
+
+
 # saludar()
 # print(saludo)
 # saludaChanchito()
